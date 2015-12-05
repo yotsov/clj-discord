6f64f39ad1163474c661cdfabcb98f97f254b427
Deyan Yotsov <deyan@yotsov.org>
2015-12-05 13:27:30 +0100

    can now obtain token and gateway

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clj-discord "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main clj-discord.core)
--- src/clj_discord/core.clj
+++ src/clj_discord/core.clj
@@ -1,10 +1,25 @@
-(ns clj-discord.core)
+(ns clj-discord.core
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
 
 ;; not committing "credentials.txt" because clojurecup 2015 repos are public
-(def username (first (.split (slurp "credentials.txt") "/")))
+(def email    (first (.split (slurp "credentials.txt") "/")))
 (def password (last  (.split (slurp "credentials.txt") "/")))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn obtain-token []
+  (let [response (client/post "https://discordapp.com/api/auth/login"
+                              {:body (json/write-str {:email email :password password})
+                               :content-type :json
+                               :accept :json})
+        status (:status response)]
+    (if (= 200 status)
+      (get (json/read-str (:body response)) "token")
+      (println "Token obtention failed with status code " status))))
+
+(defn obtain-gateway []
+  (let [response (client/get "https://discordapp.com/api/gateway"
+              {:headers {:authorization (obtain-token)}})
+        status (:status response)]
+      (if (= 200 status)
+      (get (json/read-str (:body response)) "url")
+      (println "Gateway obtention failed with status code " status))))
