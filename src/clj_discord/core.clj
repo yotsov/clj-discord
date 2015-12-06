6f90ae6e9d0f7d40944786203b3fb492e21acdd1
Deyan Yotsov <deyan@yotsov.org>
2015-12-06 14:40:00 +0100

    improved the disconnect

--- src/clj_discord/core.clj
+++ src/clj_discord/core.clj
@@ -3,11 +3,13 @@
             [clojure.data.json :as json]
             [gniazdo.core :as ws]))
 
+
 (def the-token (atom nil))
 (def the-gateway (atom nil))
 (def the-socket (atom nil))
 (def the-ready (atom nil))
 (def the-keepalive (atom nil))
+
 
 (defn obtain-token [email password]
   (let [response (client/post "https://discordapp.com/api/auth/login"
@@ -19,6 +21,7 @@
       (get (json/read-str (:body response)) "token")
       (println "Token obtention failed with status code " status))))
 
+
 (defn obtain-gateway [token]
   (let [response (client/get "https://discordapp.com/api/gateway"
                              {:headers {:authorization token}})
@@ -27,11 +30,13 @@
       (get (json/read-str (:body response)) "url")
       (println "Gateway obtention failed with status code " status))))
 
+
 (defn connect [email password]
+  (disconnect)
   (reset! the-keepalive true)
   (reset! the-token (obtain-token email password))
   (reset! the-gateway (obtain-gateway @the-token))
-  (reset! the-socket   
+  (reset! the-socket
           (ws/connect 
             @the-gateway
             :on-receive #(do 
@@ -39,14 +44,19 @@
                            (if (.equals "READY" (get (json/read-str %) "t"))
                              (reset! the-ready (json/read-str %))))))
   (ws/send-msg @the-socket (json/write-str {:op 2, :d {:token @the-token,:properties {:$browser "clj-discord"}}}))
-  (while (nil? @the-ready) (Thread/sleep 1000))
+  (while (and @the-keepalive (nil? @the-ready)) (Thread/sleep 1000))
   (.start (Thread. (fn [] (while @the-keepalive
                             (do
-                              (println (get (get @the-ready "d") "heartbeat_interval"))
                               (ws/send-msg @the-socket (json/write-str {:op 1, :d (System/currentTimeMillis)}))
-                              (Thread/sleep (get (get @the-ready "d") "heartbeat_interval"))))))))
+                              (Thread/sleep (get (get @the-ready "d") "heartbeat_interval")))))))
+  (println "Connected."))
+
 
 (defn disconnect []
   (reset! the-keepalive false)
-  (ws/close @the-socket))
+  (if (not (nil? @the-socket)) (ws/close @the-socket))
+  (reset! the-token nil)
+  (reset! the-gateway nil)
+  (reset! the-socket nil)
+  (reset! the-ready nil))
 
