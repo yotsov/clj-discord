9d378ec9bbe38b4ed7bb407ed4d508d480c80a00
Deyan Yotsov <yotsov@users.noreply.github.com>
2017-07-22 20:43:20 +0200

    setting version back to how it was

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-discord "0.1.1"
+(defproject clj-discord "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.3.0"]
                  [org.clojure/data.json "0.2.6"]
