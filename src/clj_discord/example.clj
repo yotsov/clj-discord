12d7e2da505415b78d9c3e20ce1c2d7cbaab46d2
Deyan Yotsov <deyan@yotsov.org>
2021-12-25 07:14:21 +0100

    adding linting script and new example location

---
+++ src/clj_discord/example.clj
@@ -0,0 +1,20 @@
+(ns clj-discord.example
+  (:gen-class)
+  (:require [clj-discord.core :as discord]))
+
+(defonce token (.trim (slurp "token.txt")))
+
+(defn d100 [type data]
+  (discord/answer-command data "!d100" (str "Here you are a random number between 1 and 100: " (inc (rand-int 100)))))
+
+(defn log-event [type data]
+  (println "\nReceived: " type " -> " data))
+
+(defn -main []
+  (discord/connect {:token token
+                    :functions {"MESSAGE_CREATE" [d100]
+                                "MESSAGE_UPDATE" [d100]
+                                ; "ALL_OTHER" [log-event]
+                                }}))
+
+;(discord/disconnect)
